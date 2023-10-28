ffdceecc6fd72f16c3a6a77d7461c0b15857ff22
AnasGrzor <anaskhalid658@gmail.com>
2023-10-29 01:26:07 +0500

    updated ui

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -1,8 +1,18 @@
 const form = document.querySelector('form');
 const card_1 = document.querySelector('.card');
 const card_2 = document.querySelector('.card-2');
+const email = document.querySelector('#email');
+const dismiss = document.querySelector('.dismiss');
+let userEmail = document.querySelector('.userEmail');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     card_1.classList.add("hide")
     card_2.classList.remove("hide")
+    userEmail.innerHTML = email.value
 })
+
+dismiss.addEventListener("click",(e) => {
+    email.value = "";
+    card_1.classList.remove("hide")
+    card_2.classList.add("hide")
+} )
